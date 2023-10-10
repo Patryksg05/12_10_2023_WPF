015c5e09c27b0b5d44d3cf2df5f7d3fa33d3f25f
Patryk Maciak <maciakpatryk32@gmail.com>
2023-10-10 21:00:27 +0200

    random password from 4 checkboxes

--- first_app/first_app/MainWindow.xaml.cs
+++ first_app/first_app/MainWindow.xaml.cs
@@ -30,7 +30,7 @@ namespace first_app
             DataContext= this;
 
             comboBoxItems = new ObservableCollection<ComboBoxItem>();
-            var CbItem = new ComboBoxItem { Content = "SELECT VALUE" };
+            var CbItem = new ComboBoxItem { Content = "<-- SV -->" };
             selectedCbItem = CbItem;
             comboBoxItems.Add(CbItem);
 
@@ -49,11 +49,54 @@ namespace first_app
         {
             if (new_item_text_box.Text != String.Empty)
             {
+                MessageBox.Show("Added!");
                 comboBoxItems.Add(new ComboBoxItem { Content = new_item_text_box.Text.ToString() });
                 new_item_text_box.Clear();
             }
             else
                 MessageBox.Show("Fill input!");
+        }
+
+        private void generate_passowrd_btn(object sender, RoutedEventArgs e)
+        {
+            const string lower_case = "abcdefgijklmnoprstwuxyz";
+            const string upper_case = "ABCDEFGHIJKLOMNOPRSTWUXYZ";
+            const string digits = "1234567890";
+            const string special_chars = "!@#%^&*():{}|";
+
+            string result_pass = string.Empty;
+            string all_chars = string.Empty;
+            Random random = new Random();
+            int checkbox_counter = 0;
+
+            if(chb_lower.IsChecked == true)
+            {
+                all_chars += lower_case;
+                checkbox_counter++;
+            }
+            if(chb_upper.IsChecked == true)
+            {
+                all_chars += upper_case;
+                checkbox_counter++;
+            }
+            if(chb_digits.IsChecked == true)
+            {
+                all_chars += digits;
+                checkbox_counter++;
+            }
+            if(chb_special.IsChecked == true)
+            {
+                all_chars += special_chars;
+                checkbox_counter++;
+            }
+
+            // MessageBox.Show("Checkboxes: " + checkbox_counter);
+            
+            for(int i=0; i<15; i++)
+            {
+                result_pass += all_chars[random.Next(0, all_chars.Length - 1)];
+            }
+            generated_password_text_block.Text = result_pass;
         }
     }
     
