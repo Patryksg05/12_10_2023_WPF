b8913e872a6b20a4f55608b6a8e24fcfe9280e44
Patryk Maciak <maciakpatryk32@gmail.com>
2023-10-10 19:48:56 +0200

    dynamically combobox using observable collection and binding: select item, add new

--- first_app/first_app/MainWindow.xaml.cs
+++ first_app/first_app/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,30 @@ namespace first_app
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<ComboBoxItem> comboBoxItems { get; set; }
+        public ComboBoxItem selectedCbItem { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            DataContext= this;
+
+            comboBoxItems = new ObservableCollection<ComboBoxItem>();
+            var cbItem = new ComboBoxItem { Content = "<-- SELECT -->"};
+            selectedCbItem = cbItem;
+            comboBoxItems.Add(cbItem);
+
+            comboBoxItems.Add(new ComboBoxItem { Content = "Option 1"});
+            comboBoxItems.Add(new ComboBoxItem { Content = "Option 2"});
+        }
+
+        private void add_to_combobox_Click(object sender, RoutedEventArgs e)
+        {
+            comboBoxItems.Add(new ComboBoxItem { Content = text_box_combox_item.Text.ToString() });
+        }
+
+        private void select_from_combo_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(selectedCbItem.Content.ToString());
         }
     }
 }
